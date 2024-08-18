bf2421ef04936ce081c6b76085766f200b2224c5
PenguinPush <icyzminecraft@gmail.com>
2024-08-18 10:15:13 -0400

    cyclash copy link

--- js/global.js
+++ js/global.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById("overlay").style.opacity = "0";
 });
+
+function copyText() {
+    let copyText = document.getElementById("copy-link");
+
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+
+    navigator.clipboard.writeText(copyText.value);
+}
